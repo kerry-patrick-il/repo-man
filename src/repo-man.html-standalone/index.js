4c4f2793a56bbb268a1a1fefc17a147163b99368
kerry-patrick-il <146291855+kerry-patrick-il@users.noreply.github.com>
2024-12-26 21:53:55 -0600

    adding updated todo list to index.js file

--- src/repo-man.html-standalone/index.js
+++ src/repo-man.html-standalone/index.js
@@ -29,3 +29,14 @@ window.addEventListener("load", () => {
   svg.setAttribute("width", maxX + 10);
   svg.setAttribute("height", rowMaxY);
 });
+
+/* todo: 
+- add unit tests
+ - determine color of file by file extension
+ - determine intensity of color by number of revisions
+ - add a legend showing colors and their meanings
+ - add a second file radius algorithm that uses number of revisions, and create a switch between the two algorithms
+ - add a second file color algorithm that uses red to green for number of revisions, and create a switch between the two
+ - add a second color intensity algorithm that uses last revision time and create a switch between the two 
+ - add a set of toggles for changing the diagram algorithms dynamically */
+ 
