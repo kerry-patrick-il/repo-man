49560adec02d300c7cea75125150c86e04a72908
kerry-patrick-il <146291855+kerry-patrick-il@users.noreply.github.com>
2024-12-27 21:15:44 -0600

    additional file extension tests

--- src/repo-man.html-standalone/scripts/fileExtension.test.js
+++ src/repo-man.html-standalone/scripts/fileExtension.test.js
@@ -4,4 +4,16 @@ describe("file extension", () => {
   it("should return the last segment of the file", () => {
     expect(getFileExtension("example.txt")).toBe("txt");
   });
+
+  it("should return everything but the period for gitignore", () => {
+    expect(getFileExtension(".gitignore")).toBe("gitignore");
+  });
+
+  it("should return the whole file name if there's no period", () => {
+    expect(getFileExtension("example")).toBe("example");
+  });
+
+  it("should handle multiple periods in the file name", () => {
+    expect(getFileExtension("get.me.the.extension.txt")).toBe("txt");
+  });
 });
