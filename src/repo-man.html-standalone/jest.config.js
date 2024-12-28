abb38d595a9d5dbcf081668114bc4a021eae66fb
kerry-patrick-il <146291855+kerry-patrick-il@users.noreply.github.com>
2024-12-28 10:17:31 -0600

    fixing npm build and npm test issue

--- src/repo-man.html-standalone/jest.config.js
+++ src/repo-man.html-standalone/jest.config.js
@@ -4,7 +4,7 @@
  */
 
 /** @type {import('jest').Config} */
-const config = {
+module.exports = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
@@ -198,4 +198,3 @@ const config = {
   // watchman: true,
 };
 
-export default config;
