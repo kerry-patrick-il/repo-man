e7f8aad8c0ec8732cc8e226845a6a518f8c762d7
kerry-patrick-il <146291855+kerry-patrick-il@users.noreply.github.com>
2024-12-29 22:46:29 -0600

    tdd-ing in color intensity logic

--- src/repo-man.html-standalone/scripts/getColorIntensityByRevisions.js
+++ src/repo-man.html-standalone/scripts/getColorIntensityByRevisions.js
@@ -1,3 +1,17 @@
 export function getColorIntensityByRevisions(file, bounds) {
-  return (255).toString(16).padStart(2, "0");
+  let result = bounds.maxOutput;
+  const commits = file.commits;
+  if (commits >= bounds.maxInput) {
+    result = bounds.maxOutput;
+  } else if (commits <= bounds.minInput) {
+    result = bounds.minOutput;
+  } else {
+    result =
+      ((commits - bounds.minInput) / (bounds.maxInput - bounds.minInput)) *
+        (bounds.maxOutput - bounds.minOutput) +
+      bounds.minOutput;
+  }
+
+  return result.toString(16).padStart(2, "0");
+  
 }
--- src/repo-man.html-standalone/scripts/getColorIntensityByRevisions.test.js
+++ src/repo-man.html-standalone/scripts/getColorIntensityByRevisions.test.js
@@ -7,13 +7,12 @@ describe("get color intensity by revisions", () => {
       { minInput: 0, maxInput: 10, minOutput: 100, maxOutput: 255 }
     );
     expect(result.length).toBe(2);
-    //cast the string to an integer
     const intValue = parseInt(result, 16);
     expect(intValue).toBeGreaterThan(-1);
     expect(intValue).toBeLessThan(256);
   });
 
-  it("should return 255 if the file has the max commits in the repo", () => {
+  it("should return max output if the file has the max commits in the repo", () => {
     const bounds = {
       minInput: 1,
       maxInput: 10,
@@ -23,5 +22,31 @@ describe("get color intensity by revisions", () => {
 
     const intensity = getColorIntensityByRevisions({ commits: 10 }, bounds);
     expect(intensity).toBe("ff");
+  });
+
+  it("should return min output if the file has the min commits in the repo", () => {
+    const bounds = {
+      minInput: 1,
+      maxInput: 10,
+      minOutput: 100,
+      maxOutput: 255,
+    };
+
+    const intensity = getColorIntensityByRevisions({ commits: 1 }, bounds);
+    expect(intensity).toBe("64");
+  });
+
+  it("should return some intensity between min and max if the file has a number of commits between min and max", () => {
+    const bounds = {
+      minInput: 1,
+      maxInput: 10,
+      minOutput: 100,
+      maxOutput: 255,
+    };
+
+    const intensity = getColorIntensityByRevisions({ commits: 7 }, bounds);
+    const intValue = parseInt(intensity, 16);
+    expect(intValue).toBeGreaterThan(100);
+    expect(intValue).toBeLessThan(255);
   });
 });
