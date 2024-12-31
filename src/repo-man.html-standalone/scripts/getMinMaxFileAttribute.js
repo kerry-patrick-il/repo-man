7c24b9dde51efc7e1935635ffef3485aac512cb2
kerry-patrick-il <146291855+kerry-patrick-il@users.noreply.github.com>
2024-12-30 23:34:49 -0600

    adding unit tests for get min file value, discovered a problem with the logic that needed to be fixed

--- src/repo-man.html-standalone/scripts/getMinMaxFileAttribute.js
+++ src/repo-man.html-standalone/scripts/getMinMaxFileAttribute.js
@@ -2,17 +2,21 @@ export function getMaxFileAttribute(repoInfo, attrib = "size") {
   return Math.max(
     0,
     ...(repoInfo.files?.map((f) => f[attrib]) ?? []),
-    ...(repoInfo.folders?.flatMap((folder) => getMaxFileAttribute(folder)) ??
-      [])
+    ...(repoInfo.folders?.flatMap((folder) =>
+      getMaxFileAttribute(folder, attrib)
+    ) ?? [])
   );
 }
 
 export function getMinFileAttribute(repoInfo, attrib = "size") {
-  return (
-    Math.min(
-      ...(repoInfo.files?.map((f) => f[attrib]) ?? []),
-      ...(repoInfo.folders?.flatMap((folder) => getMinFileAttribute(folder)) ??
-        [])
-    ) ?? 0
-  );
+  const attribValues = [
+    ...(repoInfo.files?.map((f) => f[attrib]) ?? [Infinity]),
+    ...(repoInfo.folders?.flatMap((folder) =>
+      getMinFileAttribute(folder, attrib)
+    ) ?? [Infinity]),
+  ].filter((x) => !isNaN(x) && isFinite(x));
+
+  const result = attribValues.length > 0 ? Math.min(...attribValues) : 0;
+
+  return result;
 }
--- src/repo-man.html-standalone/scripts/getMinMaxFileAttribute.test.js
+++ src/repo-man.html-standalone/scripts/getMinMaxFileAttribute.test.js
@@ -1,4 +1,7 @@
-import { getMaxFileAttribute } from "./getMinMaxFileAttribute";
+import {
+  getMaxFileAttribute,
+  getMinFileAttribute,
+} from "./getMinMaxFileAttribute";
 
 describe("get max file size (default attribute)", () => {
   it("should return 0 for an empty repoInfo", () => {
@@ -20,5 +23,54 @@ describe("get max file size (default attribute)", () => {
       folders: [{ files: [{ size: 400 }, { size: 200 }] }],
     };
     expect(getMaxFileAttribute(repoInfo)).toBe(400);
+  });
+
+  it("should return the max of some other attribute if requested", () => {
+    const repoInfo = {
+      files: [{ commits: 20 }, { commits: 25 }],
+      folders: [
+        { files: [{ commits: 30 }, { commits: 18 }] },
+        { files: [{ commits: 27 }] },
+      ],
+    };
+
+    const result = getMaxFileAttribute(repoInfo, "commits");
+    expect(result).toBe(30);
+  });
+});
+
+describe("get min file size (default attribute)", () => {
+  it("should return 0 for an empty repoInfo", () => {
+    const repoInfo = {};
+    expect(getMinFileAttribute(repoInfo)).toBe(0);
+  });
+
+  it("should return the min file size from the files", () => {
+    const repoInfo = {
+      files: [{ size: 100 }, { size: 200 }, { size: 150 }],
+    };
+
+    expect(getMinFileAttribute(repoInfo)).toBe(100);
+  });
+
+  it("should return the min file size from the folders if lesser than the files", () => {
+    const repoInfo = {
+      files: [{ size: 100 }, { size: 130 }, { size: 110 }],
+      folders: [{ files: [{ size: 40 }, { size: 20 }] }],
+    };
+    expect(getMinFileAttribute(repoInfo)).toBe(20);
+  });
+
+  it("should return the min of some other attribute if requested", () => {
+    const repoInfo = {
+      files: [{ commits: 20 }, { commits: 25 }],
+      folders: [
+        { files: [{ commits: 30 }, { commits: 18 }] },
+        { files: [{ commits: 27 }] },
+      ],
+    };
+
+    const result = getMinFileAttribute(repoInfo, "commits");
+    expect(result).toBe(18);
   });
 });
