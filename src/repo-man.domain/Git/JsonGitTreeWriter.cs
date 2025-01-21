95993fe674516d4075a7965390f08baf89ad460b
Kerry Patrick <softwareliberationarmy@gmail.com>
2025-01-21 07:50:53 -0600

    driving out the JSON serialization logic, could use some more test cases in the serialization output. Separate out that logic?

--- src/repo-man.domain/Git/JsonGitTreeWriter.cs
+++ src/repo-man.domain/Git/JsonGitTreeWriter.cs
@@ -1,9 +1,87 @@
-﻿namespace repo_man.domain.Git;
+﻿using System.Text.Json;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using repo_man.domain.FileSystem;
 
-public class JsonGitTreeWriter
+namespace repo_man.domain.Git;
+
+public class JsonGitTreeWriter(IConfiguration config, IFileSystem fileSystem, ILogger<JsonGitTreeWriter> logger)
 {
-    public virtual Task WriteTreeToFile(GitTree tree)
+    public virtual async Task WriteTreeToFile(GitTree tree)
     {
-        throw new NotImplementedException();
+        var json = GetTreeAsJson(tree);
+
+        var codeFileText = $"const repoInfo = {json};";
+
+        var fileName= config["GitTreeJsonFile"];
+        if (string.IsNullOrEmpty(fileName))
+        {
+            logger.LogError("Invalid output filename. GitTreeJsonFile configuration value must be provided.");
+            return;
+        }
+
+        await fileSystem.WriteTextToFileAsync(codeFileText, fileName!);
     }
+
+    private string GetTreeAsJson(GitTree tree)
+    {
+        var treeOutput = new
+        {
+            tree.Name,
+            Files = tree.Files.Select(ToJsonGitFile).ToArray(),
+            Folders = tree.Folders.Select(ToJsonGitFolder).ToArray()
+        };
+
+        var json = JsonSerializer.Serialize(treeOutput, new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        });
+        return json;
+    }
+
+    private JsonGitFolder ToJsonGitFolder(GitFolder f)
+    {
+        return new JsonGitFolder(
+            f.Name,
+            f.Files.Select(ToJsonGitFile).ToArray(),
+            f.Folders.Select(ToJsonGitFolder).ToArray());
+    }
+
+    private JsonGitFile ToJsonGitFile(GitFile file)
+    {
+        return new JsonGitFile(
+            file.Name,
+            file.FileSize,
+            file.Commits.Count,
+            (DateTimeOffset.UtcNow - (file.Commits.Max(c => c.CommitDate) ?? DateTimeOffset.UtcNow)).Days
+        );
+    }
+}
+
+public class JsonGitFile(string name, long size, int commits, int lastCommit)
+{
+    [UsedImplicitly]
+    public string Name { get; } = name;
+
+    [UsedImplicitly]
+    public long Size { get; } = size;
+
+    [UsedImplicitly]
+    public int Commits { get; } = commits;
+
+    [UsedImplicitly]
+    public int LastCommit { get; } = lastCommit;
+}
+
+public class JsonGitFolder(string name, JsonGitFile[] files, JsonGitFolder[] folders)
+{
+    [UsedImplicitly]
+    public string Name { get; } = name;
+
+    [UsedImplicitly]
+    public JsonGitFile[] Files { get; } = files;
+
+    [UsedImplicitly]
+    public JsonGitFolder[] Folders { get; } = folders;
 }
---
+++ test/repo-man.xunit/domain/Diagram/JsonGitTreeWriterTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Moq;
+using repo_man.domain.FileSystem;
+using repo_man.xunit._extensions;
+
+namespace repo_man.xunit.domain.Diagram
+{
+    public class JsonGitTreeWriterTest : TestBase
+    {
+        [Fact]
+        public async Task WriteTreeToFile()
+        {
+            // Arrange
+            var expectedContent = "const repoInfo = {\"name\":\"\",\"files\":[{\"name\":\"readme.md\",\"size\":100,\"commits\":1,\"lastCommit\":0}],\"folders\":[]};";
+            var expectedFilePath = "C:\\Temp\\output.json";
+            _mocker.GetMock<IConfiguration>().Setup(c => c["GitTreeJsonFile"]).Returns(expectedFilePath);
+            var tree = new GitTree();
+            tree.AddFile("readme.md", 100, [new Commit("123456", DateTimeOffset.Now, "Bob", "Hello")]);
+            _mocker.GetMock<ITreeExtracter>().Setup(t => t.GetFileTree()).Returns(tree);
+            var target = _mocker.CreateInstance<JsonGitTreeWriter>();
+            
+            // Act
+            await target.WriteTreeToFile(tree);
+
+            // Assert
+            _mocker.Verify<IFileSystem>(fs => fs.WriteTextToFileAsync(expectedContent, expectedFilePath));
+        }
+
+        [Fact]
+        public async Task LogsErrorWhenNoConfigFileNameProvided()
+        {
+            _mocker.GetMock<IConfiguration>().Setup(c => c["GitTreeJsonFile"]).Returns(null as string);
+
+            var target = _mocker.CreateInstance<JsonGitTreeWriter>();
+            var tree = new GitTree();
+            await target.WriteTreeToFile(tree);
+
+            _mocker.GetMock<ILogger<JsonGitTreeWriter>>().VerifyErrorWasCalled(msg => msg == "Invalid output filename. GitTreeJsonFile configuration value must be provided.", Times.Once());
+
+            _mocker.Verify<IFileSystem>(fs => fs.WriteTextToFileAsync(It.IsAny<string>(), It.IsAny<string>()), Times.Never());
+        }
+    }
+}
